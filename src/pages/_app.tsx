4a49cb1f2b5224f7557c9b14eb7c98becd78c942
Juliano Dotto <juliano.dotto@stoom.com.br>
2021-02-10 23:27:41 -0300

    pwa

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,6 +8,8 @@ function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>React Boilerplate</title>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="description" content="React w/ Next Boilerplate" />
         <link rel="shortcut icon" href="img/icon-512.png" />
         <link rel="apple-touch-icon" href="img/icon-512.png" />
       </Head>
