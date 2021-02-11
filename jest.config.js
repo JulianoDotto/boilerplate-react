7523191ee02d7ffa7a91293c72edb6f2a43e31a4
Juliano Dotto <juliano.dotto@stoom.com.br>
2021-02-10 23:37:53 -0300

    fix coverage test

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,10 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)', 'src/**/*.js(x)'],
+  collectCoverageFrom: [
+    'src/**/*.ts(x)?',
+    'src/**/*.js(x)',
+    '!src/**/stories.tsx',
+  ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
 };
